37ba1758dc957caab89f38a1c0095d93db59b91e
a7mdt <125561779+a7mdt@users.noreply.github.com>
2024-11-20 21:17:28 +0200

    Add files via upload

---
+++ Session2.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// -----------------------------------------------------------
+	
+	// Declaring & init Variables:
+
+	/*	
+		int x;
+		x = -20;
+
+		int x = 30;
+
+		int y = 20;
+		y = 30;
+
+		//int Ahmed Mohamed = 20; // Error Because of Space
+	*/
+
+	// -----------------------------------------------------------
+
+	// Rules of Naming Variables:
+
+	/*
+		//  1- Allowed Characters:
+			int AhmedTarek = 20;
+			int _AhmedTarek;
+			//int 1AhmedTarek; // Error start with number
+
+		// 2- No Reserved Keywords :
+			//int namespace = 20; // Error Reserved Keywords (namespace)
+			//int int = 20; // Error Reserved Keywords (int)
+
+		// 3- No Special Characters:
+			//int Ahmed&Mohamed = 20; // Error &
+			//int Ahmed/Mohamed = 20; // Error / 
+			//int Ahmed#Mohamed = 20; // Error #
+			//int Ahmed!Mohamed = 20; // Error !
+
+		// 4- Case Sensitivity:
+			int aaa = 20;
+			int AAA = 20;
+			int aAa = 20;
+			int aAA = 20;
+			int aaA = 20;
+			int aaA = 20;
+	*/
+
+	// -----------------------------------------------------------
+
+	// Data Types:
+
+	/*
+		// Integer Data Types:
+
+			int a = 10;
+			int b = 20;
+			int c = 1050248662;
+			int d = 2147483647;
+			long l1 = 2147483647;
+			long l2 = 21474836482;
+			long long e = 2147483657;
+
+			cout << "--------------------\n";
+
+			cout << sizeof(a) << endl;
+			cout << sizeof(e) << endl;
+			cout << sizeof(l1) << endl;
+			cout << sizeof(l2) << endl;
+
+			cout << "--------------------\n";
+
+			cout << a << endl;
+			cout << b << endl;
+			cout << c << endl;
+			cout << d << endl;
+			cout << e << endl;
+
+			cout << "--------------------\n";
+	*/
+
+	/*
+		// Floating Data Types:
+		// SEARCH!! => Why the output isn't what I expected?
+
+			float f1 = 10.12345;
+			float f2 = 10.12345678;
+
+			double d1 = 1000.123456789;
+
+			cout << sizeof(f1) << endl;
+			cout << sizeof(d1) << endl;
+
+			cout << "--------------------\n";
+
+			cout << f1 << endl;
+			cout << f2 << endl;
+			cout << d1 << endl;
+
+			cout << "--------------------\n";
+	*/
+
+	/*
+		// Characters and Words
+
+			char charA = 'A';
+			string myName = "Ahmed";
+
+			cout << sizeof(charA) << endl;
+
+			cout << "--------------------\n";
+
+			cout << charA << endl;
+			cout << myName << endl;
+
+			string name;
+			cout << "Enter ur Name : ";
+			cin >> name;
+			cout << "Welcome, " << name;
+
+			cout << "--------------------\n";
+	*/
+
+	/*
+		// Boolean Data Types:
+			bool b1 = true;
+			bool b2 = false;
+
+			cout << sizeof(b1) << endl;
+			cout << sizeof(b2) << endl;
+
+			cout << "--------------------\n";
+
+			cout << b1 << endl;
+			cout << b2 << endl;
+	*/
+
+	// -----------------------------------------------------------
+}
